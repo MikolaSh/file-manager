86c4743c86b1b876a3a6e6ffd6739a3d33a5fd0f
Mikola <nikolashulga2000@gmail.com>
2025-05-05 22:58:15 +0300

    feat: implements navigation

--- src/index.js
+++ src/index.js
@@ -1,47 +1,71 @@
 import fs from "fs";
-import path, { join } from "path";
-import { fileURLToPath } from 'url';
+import os from "os";
+import listFiles from "./navigation/listFiles.js";
+import cd from "./navigation/cd.js";
+import up from "./navigation/up.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-const curPath = join(__dirname);
-
+let currentDir = os.homedir();
 
 const getUsername = (args) => {
-
-
-
   const usernameArg = args.find(arg => arg.startsWith('--username='));
   if (usernameArg) {
    return usernameArg.split('=')[1];
   } else {
     return 'Username'
   }
-
 }
 
+const displayCurrentDirectory = () => {
+    console.log(`You are currently in ${currentDir}`);
+}
 
+const handleCommand = async (value) => {
+  const [command, ...args] = value.toString().trim().split(' ');
+  console.log(command);
+
+  switch(command) {
+    case 'cd': 
+      try {
+          currentDir = await cd(currentDir, args);
+      } catch (error) {
+          console.error(error);
+      }
+      break;
+    case 'up': 
+      currentDir = up(currentDir);
+      break;
+    case 'ls': 
+      await listFiles(currentDir);
+      break;
+    default:
+      console.log('Invalid input');
+  }
+  displayCurrentDirectory();
+}
 
 const initFileManager = () => {
   let args = process.argv.slice(2);
 
   const username = getUsername(args);
 
+  if(username) {
+    console.log(`Welcome to the File Manager, ${username}!`)
+  }
+
+  displayCurrentDirectory();
+
   const exitFileManager = () => {
     console.log(`Thank you for using File Manager, ${username}, goodbye!`);
     process.exit();
   }
 
-  if(username) {
-    console.log(`Welcome to the File Manager, ${username}!`)
-  }
 
   process.stdin.on('data', (input) => {
 
-    const userInput = input.toString().trim();
+    handleCommand(input)
 
-    if (userInput === '.exit') {
+    if (input.toString().trim() === '.exit') {
       exitFileManager()
     }
   });
